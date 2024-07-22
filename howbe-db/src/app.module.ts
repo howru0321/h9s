ec669bd476824d264516dbf3bbe793e268c0e789
Hawoo Jung <howru0321@postech.ac.kr>
2024-07-22 15:23:31 +0900

    connect sqlite

--- howbe-db/src/app.module.ts
+++ howbe-db/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { podDBConfig, replicasetDBConfig, deploymentDBConfig } from './typeorm.config';
 
 @Module({
   imports: [
@@ -9,11 +11,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       envFilePath: ['.env'],
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot(podDBConfig),//pod.sqlite 연결
+    TypeOrmModule.forRoot(replicasetDBConfig),//replicaset.sqlite 연결
+    TypeOrmModule.forRoot(deploymentDBConfig),//deploymentDBConfig 연결
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    ConfigService
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ howbe-db/src/interfaces/entity.interface.ts
@@ -0,0 +1,55 @@
+export interface PodMetadata {
+  name : string;
+  podLabels : Label[];
+  replicaset : string;
+  workernode : string;
+  containers : number;
+  containeridlist : ContainerIdInfo[];
+}
+
+export interface DeploymentMetadata {
+  name : string;
+  replicasetid : string;
+  strategyType : string;
+}
+
+export interface ReplicasetMetadata {
+  name : string;
+  replicas : number;
+  matchlabel : Label[];
+  podidlist : string[];
+  podtemplate : PodTemplate;
+}
+
+export interface PodTemplate {
+  name : string;
+  containerlist : ContainerInfo[];
+}
+
+export class CreatePodDto {
+  podName: string;
+  podLabels : Label[];
+  containerInfolist: ContainerInfo[];
+}
+
+export class Label {
+  key: string;
+  value: string;
+}
+
+export class ContainerInfo {
+  name: string;
+  image: string;
+}
+
+export class ContainerIdInfo {
+  id : string;
+  metadata : ContainerInfo;
+}
+
+export class CreateReplicasetDto {
+  replicasetName: string;
+  matchLabels : Label[];
+  replicas: number;
+  podInfo : CreatePodDto;
+}
