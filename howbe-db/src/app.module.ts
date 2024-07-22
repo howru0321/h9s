b25bc5e56fe68dcace5c44c4eb3d6608e2d6ac10
Hawoo Jung <howru0321@postech.ac.kr>
2024-07-22 14:43:39 +0900

    set up howbe-db

---
+++ howbe-db/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    ConfigService
+  ],
+})
+export class AppModule {}
