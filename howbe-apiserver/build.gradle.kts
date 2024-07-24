b1f556bb4ad3e30f2c4d8c9a72bc76c5c18fee19
Hawoo Jung <howru0321@postech.ac.kr>
2024-07-25 00:03:48 +0900

    set up gRPC in howbe-apiserver

--- howbe-apiserver/build.gradle.kts
+++ howbe-apiserver/build.gradle.kts
@@ -1,12 +1,16 @@
+import com.google.protobuf.gradle.*
+
+val grpcVersion by extra("3.25.3")
+val protobufVersion by extra("1.62.2")
+val protobufKotlinVersion by extra("1.4.1")
+
 plugins {
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
+    id("com.google.protobuf") version "0.9.4"
 }
-
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
@@ -18,6 +22,7 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -25,14 +30,41 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("io.grpc:grpc-kotlin-stub:$protobufKotlinVersion")
+    implementation("io.grpc:grpc-protobuf:$protobufVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$grpcVersion")
 }
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+sourceSets {
+    getByName("main") {
+        java {
+            srcDirs(
+                "build/generated/source/proto/main/java",
+                "build/generated/source/proto/main/kotlin"
+            )
+        }
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$grpcVersion"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$protobufVersion"
+        }
+        create("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$protobufKotlinVersion:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+                create("grpckt")
+            }
+            it.builtins { create("kotlin") }
+        }
+    }
 }
--- howbe-apiserver/src/main/kotlin/com/example/howbeapiserver/HowbeApiserverApplication.kt
+++ howbe-apiserver/src/main/kotlin/com/example/howbeapiserver/HowbeApiserverApplication.kt
@@ -3,9 +3,26 @@ package com.example.howbeapiserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+import io.grpc.Server
+import io.grpc.ServerBuilder
+
 @SpringBootApplication
 class HowbeApiserverApplication
 
 fun main(args: Array<String>) {
-    runApplication<HowbeApiserverApplication>(*args)
+    //runApplication<HowbeApiserverApplication>(*args)
+    val server: Server = ServerBuilder.forPort(50051)
+        .addService(HellogRPC())
+        .build()
+        .start()
+
+    println("Server started, listening on 50051")
+
+    Runtime.getRuntime().addShutdownHook(Thread {
+        println("Shutting down gRPC server")
+        server.shutdown()
+        println("Server shut down")
+    })
+
+    server.awaitTermination()
 }
